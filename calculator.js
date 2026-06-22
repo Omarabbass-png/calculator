1f61243b0fdff75212187c9702022c2fabdd7a77
Omarabbass-png <omarabbass2007@gmail.com>
2026-06-22 04:21:41 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,11 +3,18 @@ const display = document.getElementById('display');
 
 function toDisplay(input){
     display.value += input;
+     if (justCalculated) {
+        display.value = input;
+        justCalculated = false;
     
-}
+}}
 function clearDisplay(){
     display.value = "";
+    
+
 }
 function calculate(){
     display.value = eval(display.value);
+     justCalculated = true;
+    
 }
